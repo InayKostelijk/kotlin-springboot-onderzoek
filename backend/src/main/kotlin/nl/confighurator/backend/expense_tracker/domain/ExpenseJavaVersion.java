67d00429feab12fd64e1b8a315e210f356e55aed
Inay Kostelijk <inay.kostelijk@student.hu.nl>
2026-01-15 19:55:14 +0100

    fix docker not runnign

--- backend/src/main/kotlin/nl/confighurator/backend/expense_tracker/domain/ExpenseJavaVersion.java
+++ backend/src/main/kotlin/nl/confighurator/backend/expense_tracker/domain/ExpenseJavaVersion.java
@@ -1,86 +1,86 @@
-package nl.confighurator.backend.expense_tracker.domain;
-
-import jakarta.persistence.*;
-import nl.confighurator.backend.expense_tracker.domain.enums.ExpenseCategory;
-import nl.confighurator.backend.user.domain.User;
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
-import java.time.LocalDateTime;
-
-
-    @Entity
-    public class ExpenseJavaVersion {
-
-        @Id
-        @GeneratedValue
-        private long id;
-        private String title;
-
-        @Enumerated(EnumType.STRING)
-        private ExpenseCategory category;
-
-        private Double price;
-
-        @CreationTimestamp
-        private LocalDateTime createdAt;
-
-        @ManyToOne(targetEntity =  User.class)
-        private User user;
-
-        @UpdateTimestamp
-        private LocalDateTime updatedAt;
-
-        public ExpenseJavaVersion() {
-        }
-        public void update(String title, ExpenseCategory category, Double price, LocalDateTime createdAt, LocalDateTime updatedAt) {
-            this.title = title;
-            this.category = category;
-            this.price = price;
-            this.createdAt = createdAt;
-            this.updatedAt = updatedAt;
-        }
-
-        public ExpenseJavaVersion(LocalDateTime updatedAt, User user, LocalDateTime createdAt, Double price, ExpenseCategory category, String title, long id) {
-            this.updatedAt = updatedAt;
-            this.user = user;
-            this.createdAt = createdAt;
-            this.price = price;
-            this.category = category;
-            this.title = title;
-            this.id = id;
-        }
-
-        public long getId() {
-            return id;
-        }
-
-        public String getTitle() {
-            return title;
-        }
-
-        public ExpenseCategory getCategory() {
-            return category;
-        }
-
-        public Double getPrice() {
-            return price;
-        }
-
-        public LocalDateTime getCreatedAt() {
-            return createdAt;
-        }
-
-        public LocalDateTime getUpdatedAt() {
-            return updatedAt;
-        }
-
-        public User getUser() {
-            return user;
-        }
-    }
-
-
-
-
-
+//package nl.confighurator.backend.expense_tracker.domain;
+//
+//import jakarta.persistence.*;
+//import nl.confighurator.backend.expense_tracker.domain.enums.ExpenseCategory;
+//import nl.confighurator.backend.user.domain.User;
+//import org.hibernate.annotations.CreationTimestamp;
+//import org.hibernate.annotations.UpdateTimestamp;
+//
+//import java.time.LocalDateTime;
+//
+//
+//    @Entity
+//    public class ExpenseJavaVersion {
+//
+//        @Id
+//        @GeneratedValue
+//        private long id;
+//        private String title;
+//
+//        @Enumerated(EnumType.STRING)
+//        private ExpenseCategory category;
+//
+//        private Double price;
+//
+//        @CreationTimestamp
+//        private LocalDateTime createdAt;
+//
+//        @ManyToOne(targetEntity =  User.class)
+//        private User user;
+//
+//        @UpdateTimestamp
+//        private LocalDateTime updatedAt;
+//
+//        public ExpenseJavaVersion() {
+//        }
+//        public void update(String title, ExpenseCategory category, Double price, LocalDateTime createdAt, LocalDateTime updatedAt) {
+//            this.title = title;
+//            this.category = category;
+//            this.price = price;
+//            this.createdAt = createdAt;
+//            this.updatedAt = updatedAt;
+//        }
+//
+//        public ExpenseJavaVersion(LocalDateTime updatedAt, User user, LocalDateTime createdAt, Double price, ExpenseCategory category, String title, long id) {
+//            this.updatedAt = updatedAt;
+//            this.user = user;
+//            this.createdAt = createdAt;
+//            this.price = price;
+//            this.category = category;
+//            this.title = title;
+//            this.id = id;
+//        }
+//
+//        public long getId() {
+//            return id;
+//        }
+//
+//        public String getTitle() {
+//            return title;
+//        }
+//
+//        public ExpenseCategory getCategory() {
+//            return category;
+//        }
+//
+//        public Double getPrice() {
+//            return price;
+//        }
+//
+//        public LocalDateTime getCreatedAt() {
+//            return createdAt;
+//        }
+//
+//        public LocalDateTime getUpdatedAt() {
+//            return updatedAt;
+//        }
+//
+//        public User getUser() {
+//            return user;
+//        }
+//    }
+//
+//
+//
+//
+//
